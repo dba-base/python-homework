3c2fd019f45e7def4c04076822b24718e10b82bc
haoxy <haoxiaoyu424@outlook.com>
2017-07-10 16:28:58 +0800

    decorater

--- Day04/decorator02.py
+++ Day04/decorator02.py
@@ -1,12 +1,16 @@
+__author__ = "xiaoyu hao"
+
 #  http://egon09.blog.51cto.com/9161406/1836763
 
 import time
 
+#函数加参数形成通用装饰器
+
 #高阶函数 + 嵌套函数 = 装饰器
 def timer(func):
-    def deco():
+    def deco(*args,**kwargs):
         start_time = time.time()
-        func()
+        func(*args,**kwargs)   #为了在被装饰的函数中支持输入参数
         stop_time = time.time()
         print("the func runtime is %s" %(stop_time - start_time))
     return deco   #返回deco的内存地址
@@ -21,11 +25,11 @@ def test1():
     print("in the test1")
 
 @timer
-def test2():
+def test2(name):   # 函数加上参数   test2 = timer(test2) => deco test2() => deco()
     time.sleep(3)
-    print("in the test2")
+    print("in the test2",name)
 
 
 #test1 = timer(test1)   # 把内存地址赋值给test1，就取到了内存中的内容，这样就实现了装饰器
 test1()   #---> deco()  执行test1的时候执行的是deco内嵌函数
-test2()
+test2("haoxy")
