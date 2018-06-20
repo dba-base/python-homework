2c8ab6f8ec53c364d24ead538e9304fee6db5a55
haoxiaoyu <haoxiaoyu@haoxiaoyudeMBP.lan>
2018-06-20 23:42:27 +0800

    详细信息实现1

--- LuffyMoniter/LuffyMoniter/settings.py
+++ LuffyMoniter/LuffyMoniter/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'LuffyMoniter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'luffymonitor',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'luffymonitor',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
