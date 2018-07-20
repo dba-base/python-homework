8ca93810c866cfe0fbaf666b8508851a6db55701
haoxy <haoxiaoyu424@outlook.com>
2018-07-20 17:26:32 +0800

    模板语言查询IP地址

---
+++ LuffyMoniter/web/templatetags/models_handle.py
@@ -0,0 +1,17 @@
+__author__ = "xiaoyu hao"
+
+from django import template
+from django.utils.safestring import mark_safe
+from web import models
+
+register = template.Library()
+
+@register.simple_tag
+def tablespace_part():
+    tablespace = models.Tablespace.objects.filter(time='15:47:32.310310')
+    tablespace_li = []
+    for tbs in tablespace:
+        tbs_z = [tbs.name,tbs.total_size,tbs.free_size,tbs.used_size,tbs.host_id]
+        tablespace_li.append(tbs_z)
+    print(tablespace_li)
+    return mark_safe(tablespace_li)
